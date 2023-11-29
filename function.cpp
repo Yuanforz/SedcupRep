391b15ee0ff309724a9050d39f821d7047267f9d
liaosiming050401 <147991715+liaosiming050401@users.noreply.github.com>
2023-11-29 19:26:46 +0800

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -844,20 +844,20 @@ seedcup::ActionType RangeAttack(int*pushnum,GameMsg& msg){
 	auto player = msg.players[msg.player_id];
     std::shared_ptr<Player> enemy = msg.players[FindEnemyID(msg)];
     if(*pushnum==-1){
-    	if(enemy->x==player->x&&enemy->y<player->y&&msg.grid[player->x][player->y+1].block_id==-1){
-    	*pushnum=1;//2 Attack up
+    	if(enemy->y==player->y&&enemy->x<player->x&&msg.grid[player->x+1][player->y].block_id==-1){
+    	*pushnum=1;
 	    return PLACED;
 	    }
-    	else if(enemy->x==player->x&&enemy->y>player->y&&msg.grid[player->x][player->y-1].block_id==-1){
-    	*pushnum=1;//3 Attack DOWN
+    	else if(enemy->y==player->y&&enemy->x>player->x&&msg.grid[player->x-1][player->y].block_id==-1){
+    	*pushnum=1;
 		return PLACED;
 	    } 
-	    else if(enemy->y==player->y&&enemy->x<player->x&&msg.grid[player->x+1][player->y].block_id==-1){
-	    *pushnum=1;//4 Attack LEFT
+	    else if(enemy->x==player->x&&enemy->y<player->y&&msg.grid[player->x][player->y+1].block_id==-1){
+	    *pushnum=1;
 	    return PLACED;
 		}
-	    else if(enemy->y==player->y&&enemy->x>player->x&&msg.grid[player->x-1][player->y].block_id==-1){
-	    *pushnum=1;//5 Attack RIGHT
+	    else if(enemy->x==player->x&&enemy->y>player->y&&msg.grid[player->x][player->y-1].block_id==-1){
+	    *pushnum=1;
 	    return PLACED;
 		}		
 		else{
@@ -866,19 +866,19 @@ seedcup::ActionType RangeAttack(int*pushnum,GameMsg& msg){
 		}
 	}
     else if(*pushnum==1){
-    	if(enemy->x==player->x&&enemy->y<player->y&&msg.grid[player->x][player->y+1].block_id==-1){
+    	if(enemy->y==player->y&&enemy->x<player->x&&msg.grid[player->x+1][player->y].block_id==-1){
     	*pushnum=2;//2 Attack up
 	    return MOVE_DOWN;
 	    }
-    	else if(enemy->x==player->x&&enemy->y>player->y&&msg.grid[player->x][player->y-1].block_id==-1){
+    	else if(enemy->y==player->y&&enemy->x>player->x&&msg.grid[player->x-1][player->y].block_id==-1){
     	*pushnum=3;//3 Attack DOWN
 		return MOVE_UP;
 	    } 
-	    else if(enemy->y==player->y&&enemy->x<player->x&&msg.grid[player->x+1][player->y].block_id==-1){
+	    else if(enemy->x==player->x&&enemy->y<player->y&&msg.grid[player->x][player->y+1].block_id==-1){
 	    *pushnum=4;//4 Attack LEFT
 	    return MOVE_RIGHT;
 		}
-	    else if(enemy->y==player->y&&enemy->x>player->x&&msg.grid[player->x-1][player->y].block_id==-1){
+	    else if(enemy->x==player->x&&enemy->y>player->y&&msg.grid[player->x][player->y-1].block_id==-1){
 	    *pushnum=5;//5 Attack RIGHT
 	    return MOVE_LEFT;
 		}		
